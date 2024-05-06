4716a4fb90e8bb21b0c7ee9254d26dfbfab3b7f9
Abhishek Mandal <abhishek.s.mandal@gmail.com>
2024-05-06 15:10:44 +0530

    Added Qualification Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 import ToggleColorMode from "./ToggleColorMode";
 import Header from "./components/header/Header";
@@ -8,6 +8,7 @@ import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Services from "./components/services/Services";
+import Qualification from "./components/qualification/Qualification";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -20,6 +21,7 @@ function App() {
         <Home />
         <About />
         <Services />
+        <Qualification />
       </main>
     </>
   );
---
+++ frontend/src/components/qualification/Qualification.jsx
@@ -0,0 +1,148 @@
+import React, { useState } from "react";
+import "./qualification.css";
+import {
+  CalendarMonthRounded,
+  SchoolRounded,
+  WorkRounded,
+} from "@mui/icons-material";
+
+const Qualification = () => {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
+  return (
+    <section className="qualification section">
+      <h2 className="section__title">Qualification</h2>
+      <span className="section__subtitle">My Personal Journey</span>
+
+      <div className="qualification__container container">
+        <div className="qualification__tabs">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(1)}
+          >
+            <div className="qualification__icon">
+              <SchoolRounded /> Education
+            </div>
+          </div>
+
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(2)}
+          >
+            <div className="qualification__icon">
+              <WorkRounded /> Experience
+            </div>
+          </div>
+        </div>
+
+        <div className="qualification__sections">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">
+                  Junior DevOps System Administrator
+                </h3>
+                <span className="qualification__subtitle">
+                  Sapna Technologies
+                </span>
+                <div className="qualification__calender">
+                  <CalendarMonthRounded /> 2023 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Help Desk Engineer</h3>
+                <span className="qualification__subtitle">
+                  Microsense Pvt Ltd
+                </span>
+                <div className="qualification__calender">
+                  <CalendarMonthRounded /> 2020 - 2023
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">
+                  DevOps System Administrator
+                </h3>
+                <span className="qualification__subtitle">
+                  Sapna Technologies
+                </span>
+                <div className="qualification__calender">
+                  <CalendarMonthRounded /> 2023 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">SuperHero</h3>
+                <span className="qualification__subtitle">
+                  Microsense Pvt Ltd
+                </span>
+                <div className="qualification__calender">
+                  <CalendarMonthRounded /> 2020 - 2023
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Qualification;
