93f479bd14ae41c8c0fd8796f3eb0ee65c8654e4
Abhishek Mandal <official.abhishek.mandal+coding@gmail.com>
2024-11-16 22:08:26 +0530

    Rename portfolio file name

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/abhishek-mandal.pdf";
+import CV from "../../assets/abhishek_mandal.pdf";
 import Info from "./Info";
 
 const About = () => {
